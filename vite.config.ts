21d5a4eb205f8de330bf4417b40a1b810ef52cdc
lernEarnBurn <lerners124@gmail.com>
2023-11-29 15:27:46 +0200

    ready to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: '/creater-interface/',
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/creater-interface/'
 });
